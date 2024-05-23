ad65ba77751686091cc39d868f1e87c35be92ef7
Anton Slavin <slavinant@gmail.com>
2024-05-24 01:22:46 +0300

    Remove bad denoiser, param tweaks, todo steps

--- decompose.py
+++ decompose.py
@@ -10,18 +10,19 @@ BLEN = 1024
 HLEN = 4096
 NFFT = 4096
 
+# TODO:
 
-def denoise(y, sr):
-    # Source: noise.py on github
-    cent = librosa.feature.spectral_centroid(y=y, sr=sr)
+# Probably use tsurumeso/vocal-remover to separate into *good* vocal stem + *excellent* bg
+#       -> side: probably just fork it and combine with our code to avoid unnecessary STFT etc
 
-    threshold_h = round(np.median(cent))*1.5
-    threshold_l = round(np.median(cent))*0.1
+# Extract drums from BG, processing?
+# Clean up vocal stem with EQ
+# Then apply pitch shift/stretch operations to try and match tempo + key
+# Combine samples (watch out for clipping) and run ISTFT
 
-    less_noise = AudioEffectsChain().lowshelf(gain=-20.0, frequency=threshold_l, slope=0.8).highshelf(gain=-10.0, frequency=threshold_h, slope=0.5).limiter(gain=10.0)
-    y_clean = less_noise(y)
+# GUI: PySide? 
 
-    return y_clean
+# TODO2: other CNN-based clean-up tools for vocals
 
 
 def lowpass(y, freq):
@@ -39,47 +40,44 @@ def decompose_hpcc(y, sr):
     S_full, phase = librosa.magphase(librosa.stft(y_for_vocals, center=True))
 
     # For rhythm, drums
-    H, perc = librosa.decompose.hpss(librosa.stft(y, center=True), margin=(2.0,4.2))
+    H, perc = librosa.decompose.hpss(librosa.stft(y, center=True), margin=(2.0,3.0))
 
     # Ugly, but a temp failsafe for shorter frames
     try: 
         S_filter = librosa.decompose.nn_filter(S_full,
                                        aggregate=np.median,
                                        metric='cosine',
-                                       width=int(librosa.time_to_frames(2, sr=sr)))
+                                       width=int(librosa.time_to_frames(2.5, sr=sr)))
     except:
         S_filter = librosa.decompose.nn_filter(S_full,
                                        aggregate=np.median,
                                        metric='cosine',
                                        width=int(librosa.time_to_frames(0.1, sr=sr)))
-
     S_filter = np.minimum(S_full, S_filter)
 
     # Extract vocals vs the rest
     mask_i = librosa.util.softmask(S_filter,
-                                   3 * (S_full - S_filter),
+                                   4 * (S_full - S_filter),
                                    power=2)
 
     mask_v = librosa.util.softmask(S_full - S_filter,
-                                   10 * S_filter,
+                                   9 * S_filter,
                                    power=2)
 
     S_foreground = mask_v * S_full
     S_background = mask_i * S_full
 
     y_foreground = librosa.istft(S_foreground * phase)
-    y_foreground = lowpass(y_foreground, freq=2000)
+    #y_foreground = lowpass(y_foreground, freq=2000)
     y_bass = librosa.istft(S_background * phase)
 
-    # Try to clean up drums from the vocal part
+    # Try to clean up drums from the vocal part 
     # vocal_H, _ = librosa.decompose.hpss(S_foreground, margin=2.0)
 
-    # TODO further filtering for drums, clean up with hpss?
     y_perc = librosa.istft(perc * phase)
-    # y_vocal = librosa.istft(vocal_H * phase)
-    y_vocal_clean = denoise(y_foreground, sr)
+    # y_vocal_clean = tg(S_foreground)
 
-    return (y_perc,y_vocal_clean,y_foreground,y_bass)
+    return (y_perc,y_foreground,y_foreground,y_bass)
 
 
 if __name__ == '__main__':
